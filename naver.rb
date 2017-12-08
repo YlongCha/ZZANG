cdb997e5806cb9bd12b76010491ee2595dd2e9bb
Leonardo <ckdufhd@naver.com>
2017-12-08 16:41:30 +0900

    네이트 일일 사건정보 수정

--- naver.rb
+++ naver.rb
@@ -1,9 +1,11 @@
 require 'httparty'
 require 'nokogiri'
 require 'rest-client'
-url = "http://m.app.melon.com/index.htm" #네이버 주소 저장
+url = "http://m.news.nate.com/hissue/list?isq=9704&mid=m00" #네이버 주소 저장
 response = RestClient.get(url) #url을 통해 요청
 # puts response
 doc = Nokogiri::HTML(response)
-kospi = doc.css("p.title.ellipsis")
-puts kospi
+kospi = doc.css("span.tb > strong.tit")
+kospi.each do |t|
+  puts t.text
+end
